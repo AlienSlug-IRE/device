af589695c11e9f41fac7ed9e6e0ded40f31e35b9
JosephODonoghue <josephodonoghue@gmail.com>
2014-12-14 14:28:33 +0000

    sdfdsf
    dsfsdf

--- device.js
+++ device.js
@@ -37,7 +37,8 @@ var server = http.createServer().listen(9615),
 
 socket.on('do', function(data) { 
     console.log('Device Commanded', data);
-    triggerDo({ _id: data._id }, data.value);
+    // triggerDo({ _id: data._id }, data.value);
+    serialPort.write(data.value);
 });
 
 var triggerDo = function(obj, value){
