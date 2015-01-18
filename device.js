dd4fc94929032ca8dce9939bcd5bd21efc01a4ae
JosephODonoghue <josephodonoghue@gmail.com>
2015-01-18 15:34:27 +0000

    dfg
    dgfdg

--- device.js
+++ device.js
@@ -19,8 +19,8 @@ var api = 'http://ubd-odonoghue.rhcloud.com/';
 var socketUrl = 'http://ubd-odonoghue.rhcloud.com:8000'
 // var usb_mount = '/dev/tty.usbmodemfd1411';
 // var usb_mount_led = '/dev/tty.usbmodemfd1431';
-var usb_mount = '/dev/ttyACM0';
-var usb_mount_led = '/dev/ttyACM1';
+var usb_mount = '/dev/ttyUSB0';
+var usb_mount_led = '/dev/ttyUSB1';
 
 var SerialPort = require('serialport').SerialPort,
     _ = require('underscore'),
