963ebf37c45293996b6db7de2c849f69c83ead97
JosephODonoghue <josephodonoghue@gmail.com>
2015-01-18 13:31:41 +0000

    dsf
    dsfsdf

--- device.js
+++ device.js
@@ -1,4 +1,3 @@
-
 /*
     
     *******************************************************************
@@ -16,48 +15,71 @@
 
 // --------------------------------------------------------------------
 
-var api = 'http://169.254.126.162:3000/'
-// var usb_mount =  '/dev/tty.usbmodemfd1411';
-var usb_mount =  '/dev/ttyACM0';
+var api = 'http://ubd-odonoghue.rhcloud.com/';
+var socketUrl = 'http://ubd-odonoghue.rhcloud.com:8000'
+var usb_mount = '/dev/tty.usbmodemfd1411';
+var usb_mount_led = '/dev/tty.usbmodemfd1431';
+// var usb_mount = '/dev/ttyACM0';
 
 var SerialPort = require('serialport').SerialPort,
     _ = require('underscore'),
     serialPort,
+    serialPortLED,
     receivedStr = '',
-    spInt, 
+    spInt,
     monitor = require('usb-detection');
 
 var request = require('request'),
     http = require('http'),
     async = require('async');
+console.log(socketUrl);
+
 
 var server = http.createServer().listen(9615),
     io = require('socket.io-client'),
-    socket = io.connect(api, {reconnect: true});
+    socket = io.connect(socketUrl, {
+        reconnect: true
+    });
 
-socket.on('do', function(data) { 
+socket.on('do', function(data) {
     console.log('Device Commanded', data);
-    // triggerDo({ _id: data._id }, data.value);
-    serialPort.write(data.value);
+    // triggerDo({ _id: data._id }, data.value);  
+    try {
+        serialPortLED.write(data.value);
+    } catch (err) {};
 });
 
-var triggerDo = function(obj, value){
-    socket.emit('trigger', { channel: device.uuid, io: obj, value: value });
-    _.each(device.config.config, function(config){
-        if(config.on.id === obj._id){
-            _.each(config.do, function(does){
-                var isDo = _.findWhere(device.ioConfigs, { _id: does.id });
-                if(isDo.type === 'io'){
+var triggerDo = function(obj, value) {
+    socket.emit('trigger', {
+        channel: device.uuid,
+        io: obj,
+        value: value
+    });
+    _.each(device.config.config, function(config) {
+        if (config.on.id === obj._id) {
+            _.each(config.do, function(does) {
+                var isDo = _.findWhere(device.ioConfigs, {
+                    _id: does.id
+                });
+                if (isDo.type === 'io') {
                     serialPort.write(isDo.settings.port);
-                } 
+                }
             });
         }
     });
 };
 
 var setSupportOptions = function(name, isSupported) {
-    var feature = _.findWhere(device.ioConfigs, { name: name });
-    if(isSupported){
+    var feature = _.findWhere(device.ioConfigs, {
+        name: name
+    });
+    socket.emit('report', {
+        uuid: device.uuid,
+        feature: name,
+        id: feature._id,
+        supported: isSupported
+    });
+    if (isSupported) {
         device.config.supports.push(feature._id);
     } else {
         device.config.supports = _.without(device.config.supports, feature._id);
@@ -71,27 +93,38 @@ var setSupportOptions = function(name, isSupported) {
         json: {
             supports: device.config.supports,
         }
-    }, function(err, res, body) { });
+    }, function(err, res, body) {});
+    return;
 };
 
-// NFC Detection
-var setupHardwareListeners = function(){
+// Detection
+var setupHardwareListeners = function() {
     monitor.find(9025, 32822, function(err, devices) {
-        if(devices.length > 0){
-            setSupportOptions('NFC', true);
-            return serialListener();
+        if (devices.length > 0) {
+            // return serialListener();
         }
     });
     monitor.on('add:9025:32822', function(err, devices) {
-        setSupportOptions('NFC', true);
-        socket.emit('report', { uuid: device.uuid, feature: 'NFC', supported: true });
-        return serialListener();
+        // setSupportOptions('NFC', true);
+        // socket.emit('report', {
+        //     uuid: device.uuid,
+        //     feature: 'NFC',
+        //     supported: true
+        // });
+        serialListener();
     });
     monitor.on('remove:9025:32822', function(err, devices) {
-        setSupportOptions('NFC', false);
-        socket.emit('report', { uuid: device.uuid, feature: 'NFC', supported: false });
-        return serialPort.close();
+        // setSupportOptions('NFC', false);
+        // socket.emit('report', {
+        //     uuid: device.uuid,
+        //     feature: 'NFC',
+        //     supported: false
+        // });
+        // serialPort.close();
+        // serialPortLED.close();
+        // return serialListener();
     });
+    serialListener();
 }
 
 function serialListener() {
@@ -102,30 +135,77 @@ function serialListener() {
         stopBits: 1,
         flowControl: true
     });
-    serialPort.open(function() {
+    serialPort.open(function(err) {
         serialPort.on('error', function(data) {
-            console.log('error');
+            console.log('NFC error');
+            try {
+                serialPort.close();
+            } catch (err) {}
+
         });
-        serialPort.on('data', function(data) {
-            clearInterval(spInt);
-            receivedStr += data.toString().trim();
-            receivedStr = receivedStr.replace(/(\r\n|\n|\r|\s)/gm, "");
-            spInt = setTimeout(function() {
-                try {
-                    var rfid = receivedStr.split('Classic')[1].split('No')[0];
-                    var isOn = _.findWhere(device.ioConfigs, { name: 'NFC' });
-                    if(!_.isEmpty(isOn)){
-                        triggerDo(isOn, rfid);
-                    }
-                } catch (err) {}
-                receivedStr = '';
-            }, 500);
-        });
-        serialPort.write('0');
+        if (err) {
+            console.log(err);
+            setSupportOptions('NFC', false);
+        } else {
+
+            serialPort.on('close', function(data) {
+                console.log('NFC closed');
+                setSupportOptions('NFC', false);
+            });
+            serialPort.on('data', function(data) {
+                clearInterval(spInt);
+                receivedStr += data.toString().trim();
+                receivedStr = receivedStr.replace(/(\r\n|\n|\r|\s)/gm, "");
+                spInt = setTimeout(function() {
+                    try {
+                        var rfid = receivedStr.split('Classic')[1].split('No')[0];
+
+                        var isOn = _.findWhere(device.ioConfigs, {
+                            name: 'NFC'
+                        });
+                        console.log(rfid);
+                        if (!_.isEmpty(isOn)) {
+                            triggerDo(isOn, rfid);
+                        }
+                    } catch (err) {}
+                    receivedStr = '';
+                }, 1000);
+            });
+            console.log('NFC connected');
+            setSupportOptions('NFC', true);
+        }
     });
+    serialPortLED = new SerialPort(usb_mount_led, {
+        baudrate: 9600,
+        dataBits: 8,
+        parity: 'none',
+        stopBits: 1,
+        flowControl: true
+    });
+    serialPortLED.open(function(err) {
+        serialPortLED.on('error', function(data) {
+            console.log('LED Error');
+            try {
+                serialPortLED.close();
+            } catch (err) {}
+        });
+        if (err) {
+            setSupportOptions('LED', false);
+        } else {
 
+            serialPortLED.on('close', function(data) {
+                console.log('LED closed');
+                setSupportOptions('LED', false);
+            });
+            serialPortLED.on('data', function(data) {
+                // do nothing as it's LED
+            });
+            console.log('LED connected');
+            setSupportOptions('LED', true);
+            serialPortLED.write('2');
+        }
+    });
 }
-
 
 /*
     
@@ -154,7 +234,7 @@ socket.on('connect', function(socket) {
 // --------------------------------------------------------------------
 
 var device = {
-    uuid: '54873747f5b4f6870fe5d15d',
+    uuid: '54ad9afd238a170000d7094f',
     config: {},
     ioConfigs: {}
 };
@@ -215,7 +295,7 @@ var setup = function() {
     };
     var tasks = [checkRegistered, getSupportOptions];
     async.series(tasks, function(err, results) {
-        console.log('Device Running');
+        console.log('Device Running', results);
         if (err) {
             registerDevice(function(res) {
                 device.uuid = res.device._id;
@@ -223,19 +303,13 @@ var setup = function() {
                 setup();
             })
         } else {
-            socket.emit('subscribe', { channel: device.uuid });
+            socket.emit('subscribe', {
+                channel: device.uuid
+            });
 
             setupHardwareListeners();
             setTimeout(setup, 100000);
         }
     });
 }
-
 setup();
-
-
-
-
-
-
-
